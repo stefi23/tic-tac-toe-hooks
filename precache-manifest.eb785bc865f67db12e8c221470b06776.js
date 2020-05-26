e084e4da45f7e11d26987e04b702994ef94d4fd8
stefi23 <stefania.rosca2309@gmail.com>
2020-05-26 10:58:55 +0200

    Updates

---
+++ precache-manifest.eb785bc865f67db12e8c221470b06776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaca850b0d086cf5981526ce0ff768ff",
+    "url": "/tic-tac-toe-hooks/index.html"
+  },
+  {
+    "revision": "d8949573e534ddd62db2",
+    "url": "/tic-tac-toe-hooks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95eec120f73aa666b485",
+    "url": "/tic-tac-toe-hooks/static/js/2.9308e7b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-hooks/static/js/2.9308e7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8949573e534ddd62db2",
+    "url": "/tic-tac-toe-hooks/static/js/main.49d8f838.chunk.js"
+  },
+  {
+    "revision": "db8c31f754e72b9a0a3e",
+    "url": "/tic-tac-toe-hooks/static/js/runtime-main.b2a6d5e4.js"
+  }
+]);
